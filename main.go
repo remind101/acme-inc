1ce9490a92c71ef68b1b1320b5001fe4506da271
Eric J. Holmes <eric@ejholmes.net>
2015-04-16 14:44:07 -0700

    Add a worker process.

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -14,12 +17,27 @@ func main() {
 	)
 	flag.Parse()
 
-	log.Printf("Starting on %s", *port)
-	log.Fatal(http.ListenAndServe(":"+*port, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s - %s", r.Method, r.URL)
-		w.WriteHeader(200)
-		io.WriteString(w, "Ok\n")
-	})))
+	if len(os.Args) < 2 {
+		log.Fatal("You must specify the `server` or `worker` subcommand.")
+	}
+
+	cmd := os.Args[1]
+	switch cmd {
+	case "server":
+		log.Printf("Starting on %s", *port)
+		log.Fatal(http.ListenAndServe(":"+*port, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("%s - %s", r.Method, r.URL)
+			w.WriteHeader(200)
+			io.WriteString(w, "Ok\n")
+		})))
+	case "worker":
+		for {
+			<-time.After(1 * time.Second)
+			fmt.Printf("Hard work %d...\n", rand.Int())
+		}
+	default:
+		log.Fatalf("Unkown subcommand: %s", cmd)
+	}
 }
 
 func env(key string, fallback string) string {
